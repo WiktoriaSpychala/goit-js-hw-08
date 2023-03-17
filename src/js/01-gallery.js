172ead07a3df13a92eb5761df2fb74abcc8f089a
Wiktoria Spychała <wiktoria_spychala@o2.pl>
2023-03-17 02:21:25 +0100

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,9 +9,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
 
-const galleryItem = galleryItems
-  .map(
-    item => `
+const galleryItem = galleryItems.map(item => `
 <a class = "gallery__link" href = "${item.original}">
 <img class = "gallery__image" src = "${item.preview}"
 data-source = "${item.original}" alt = "${item.description}"/></a></div>`
@@ -19,7 +17,7 @@ data-source = "${item.original}" alt = "${item.description}"/></a></div>`
 
 gallery.insertAdjacentHTML('beforeend', galleryItem);
 
-const lightbox = new SimpleLightbox('.gallery .gallery__item', {
+let lightbox = new SimpleLightbox('.gallery a', {
   captions: true,
   captionSelector: 'img',
   captionsData: 'alt',
