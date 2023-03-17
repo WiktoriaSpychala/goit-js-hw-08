708a6982f45b443c9aa13c6b5063cf880666e8c8
Wiktoria Spychała <wiktoria_spychala@o2.pl>
2023-03-17 05:00:08 +0100

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const savedForm = localStorage.getItem('feedback-form-state');
+const parsedForm = JSON.parse(savedForm);
+const throttled = throttle(savingForm, 500);
+
+function savingForm() {
+  const formValue = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formValue));
+}
+
+function clickSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  email.value = '';
+  message.value = '';
+}
+
+form.addEventListener('input', throttled);
+form.addEventListener('submit', clickSubmit);
+
+if (
+  parsedForm !== null
+) {
+  email.value = parsedForm.email;
+  message.value = parsedForm.message;
+}
